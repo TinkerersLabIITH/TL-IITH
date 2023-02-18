9265f93fb4e0edeb32c3d19c8eefe161bd9b30df
Bhanu <bhanujarpula9@gmail.com>
2023-02-18 14:50:31 +0530

    added svg

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import Logo from '../Images/Logo.svg'
+import Logo from '../Images/LogoCopy.svg'
 
 const HeroSection = () => {
   return (
@@ -9,11 +9,18 @@ const HeroSection = () => {
                 <h1 className="md:text-6xl sm:text-4xl text-6xl font-bold p-2 "> Tinkerers' Laboratory</h1>
 
                 <p>Think. Tinker. Transform</p>
+                <div className="container header_container">
+        <div className="header_left">
+            <h2>What we do?</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, commodi ab. Molestiae accusamus iure quas! Possimus alias voluptas at esse harum sed officiis ipsa voluptates earum accusamus velit, repellat quos consequuntur suscipit iusto eum sapiente facere voluptate iure neque minus!</p>
+            <NavLink className="anchor btn btn-primary" to="/contacts">Reach us</NavLink>
+        </div>
+      </div>
 
             </div>
             <div className="section-hero-image">
                 <picture>
-                    <img className='w-[300px] mx-auto my-4' src="{Logo}" alt="tinkrers-image" />
+                    <img className='w-[300px] mx-auto my-4' src={Logo} alt="tinkrers-image" />
                 </picture>
             </div>
         </div>
