f9dd232fd2aa14922a232ac6cf462a600664251f
Rahul-Aryan <rahularyan2211@gmail.com>
2023-02-22 16:28:52 +0530

    Made Team Page

--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -4,9 +4,9 @@ import ProfileCard from '../Components/ProfileCard';
 const Team = () => {
   return (
     <div className='flex flex-col items-center'>
-      <h2 className="md:text-6xl text-3xl font-bold pt-8">Our Team</h2>
-
-      <h2 className='md:text-4xl text-2xl font-bold py-8'>Head</h2>
+      <h2 className="text-6xl  font-bold pt-8">Our Team</h2>
+      <div className='flex flex-col mx-12 items-center'>
+      <h2 className='text-4xl font-bold py-8'>Head</h2>
       {/* <div className="container grid grid-one-column">
 
         <div className="card">
@@ -21,32 +21,34 @@ const Team = () => {
         
 
       </div> */}
+      
       <ProfileCard
-        title="Rahul Aryan"
+        title="Aadil Salim"
         gradient="from-[#D8B4FE] to-[#818CF8]"
         Domain="Overall Head"
-        ImagePath="src/Images/Team/RahulAryan.jpg"
+        ImagePath="src/Images/Team/Aadil.jpeg"
       />
+      </div>
 
       <h2 className='md:text-4xl text-2xl font-bold py-8'>Managers</h2>
-      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row '>
+      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row mx-12'>
       <ProfileCard
         title="Samyak Joshi"
         gradient="from-[#D8B4FE] to-[#818CF8]"
-        Domain="Overall Head"
+        Domain="Events, Inventory and Finance"
         ImagePath="src/Images/Team/Samyak.jpg"
       />
       <ProfileCard
         title="Rahul Aryan"
         gradient="from-[#D8B4FE] to-[#818CF8]"
-        Domain="Overall Head"
+        Domain="Design, PR and Outreach"
         ImagePath="src/Images/Team/RahulAryan.jpg"
       />
       <ProfileCard
-        title="Rahul Aryan"
+        title="Arpit Paida"
         gradient="from-[#D8B4FE] to-[#818CF8]"
-        Domain="Overall Head"
-        ImagePath="src/Images/Team/RahulAryan.jpg"
+        Domain="Technical Manager"
+        ImagePath="src/Images/Team/Arpit.jpeg"
       />
       </div>
       
@@ -86,201 +88,143 @@ const Team = () => {
       </div> */}
 
 
-      <h2 className='title'>Coordinators</h2>
+      <h2 className='md:text-4xl text-2xl font-bold py-8'>Coordinators</h2>
 
-      <div className="container grid grid-four-column">
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/VarunNadimpalli.jpg" alt="varun" />
-          </figure>
-          <div className="card-data">
-            <h3>Varun Nadimpalli</h3>
-            <p>Technical Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/JarpulaBhanuPrasad.jpg" alt="Bhanu" />
-          </figure>
-          <div className="card-data">
-            <h3>Jarpula Bhanu Prasad</h3>
-            <p>Technical Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/AbhayGupta.jpg" alt="Abhay" />
-          </figure>
-          <div className="card-data">
-            <h3>Abhay Kumar</h3>
-            <p>Technical Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/" alt="Bhanu" />
-          </figure>
-          <div className="card-data">
-            <h3>Pranadipan Sahoo</h3>
-            <p>Technical , Inventory <br /> and finance Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/Jagadeep.jpg" alt="jagadeep" />
-          </figure>
-          <div className="card-data">
-            <h3>Jagadeep Borra</h3>
-            <p>Event Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/some.img" alt="Bhanu" />
-          </figure>
-          <div className="card-data">
-            <h3>Rishi Cheekatla</h3>
-            <p>Event Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/TiteekshaGodambe .jpg" alt="Titeeksha" />
-          </figure>
-          <div className="card-data">
-            <h3>Titeeksha Godambe</h3>
-            <p>Event Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/PoojithaMallina.jpg" alt="Poojitha" />
-          </figure>
-          <div className="card-data">
-            <h3>Mallina Poojitha Sai </h3>
-            <p>Event Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Images/some.img" alt="Bhanu" />
-          </figure>
-          <div className="card-data">
-            <h3>Sai Geetha Bhavana</h3>
-            <p>Event Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/AnandMathapati.jpg" alt="Anand" />
-          </figure>
-          <div className="card-data">
-            <h3>Anand Mathapathi</h3>
-            <p>Inventory and finance Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/SriRamParuchuri.jpg" alt="Sriram" />
-          </figure>
-          <div className="card-data">
-            <h3>Paruchuri SriRam</h3>
-            <p>Inventory and finance Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/PriteshSingh.jpg" alt="Pritesh" />
-          </figure>
-          <div className="card-data">
-            <h3>Pritesh Singh</h3>
-            <p>Inventory and finance Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/ArshArora.jpg" alt="Arsh" />
-          </figure>
-          <div className="card-data">
-            <h3>Arsh Arora</h3>
-            <p>PR and outreach Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/AdityaGupta.jpg" alt="Aditya" />
-          </figure>
-          <div className="card-data">
-            <h3>Aditya Gupta</h3>
-            <p>PR and outreach Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/BapatuManojKumarReddy.jpg" alt="Manoj" />
-          </figure>
-          <div className="card-data">
-            <h3>Manoj Kumar Reddy</h3>
-            <p>PR and outreach Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/DevanshAgrawalpicture.jpg" alt="Devansh" />
-          </figure>
-          <div className="card-data">
-            <h3>Devansh Agrawal</h3>
-            <p>PR and outreach Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/ArinKulkarni.jpg" alt="Arin" />
-          </figure>
-          <div className="card-data">
-            <h3>Arin Kulkarni</h3>
-            <p>Design Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/BanavathVishnu.jpg" alt="vishnu" />
-          </figure>
-          <div className="card-data">
-            <h3>Banavath Vishnu</h3>
-            <p>Design Coordinator</p>
-          </div>
-        </div>
-
-        <div className="card">
-          <figure>
-            <img src="src/Images/Team/TanishReddy_.jpg" alt="Tanish" />
-          </figure>
-          <div className="card-data">
-            <h3>Tanish Samikommu</h3>
-            <p>Design Coordinator</p>
-          </div>
-        </div>
-
+      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row mx-12'>
+      <ProfileCard
+        title="Varun Nadimpalli"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Technical Coordinator"
+        ImagePath="src/Images/Team/VarunNadimpalli.jpg"
+      />
+      <ProfileCard
+        title="Jarpula Bhanu Prasad"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Technical Coordinator"
+        ImagePath="src/Images/Team/JarpulaBhanuPrasad.jpg"
+      />
+      <ProfileCard
+        title="Abhay Kumar"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Technical Coordinator"
+        ImagePath="src/Images/Team/AbhayGupta.jpg"
+      />
       </div>
+
+      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row mx-12'>
+      <ProfileCard
+        title="Pranadipan Sahoo"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Technical, Finance, Inventory"
+        ImagePath="src/Images/Team/Pranadeepan.png"
+      />
+      <ProfileCard
+        title="Jagadeep Borra"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Event Coordinator"
+        ImagePath="src/Images/Team/Jagadeep.jpg"
+      />
+      <ProfileCard
+        title="Rishi Cheekatla"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Event Coordinator"
+        ImagePath="src/Images/Team/AbhayGupta.jpg"
+      />
+      </div>
+
+      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row mx-12'>
+      <ProfileCard
+        title="Titeeksha Godambe"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Event Coordinator"
+        ImagePath="src/Images/Team/TiteekshaGodambe .jpg"
+      />
+      <ProfileCard
+        title="Mallina Poojitha Sai"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Event Coordinator"
+        ImagePath="src/Images/Team/PoojithaMallina.jpg"
+      />
+      <ProfileCard
+        title="Sai Geetha Bhavana"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Event Coordinator"
+        ImagePath="src/Images/Team/GeethaBhavana.jpeg"
+      />
+      </div>
+
+      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row mx-12'>
+      <ProfileCard
+        title="Anand Mathapathi"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Inventory and finance Coordinator"
+        ImagePath="src/Images/Team/AnandMathapati.jpg"
+      />
+      <ProfileCard
+        title="Paruchuri SriRam"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Inventory and finance Coordinator"
+        ImagePath="src/Images/Team/SriRamParuchuri.jpg"
+      />
+      <ProfileCard
+        title="Pritesh Singh"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Inventory and finance Coordinator"
+        ImagePath="src/Images/Team/PriteshSingh.jpg"
+      />
+      </div>
+
+      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row mx-12'>
+      <ProfileCard
+        title="Arsh Arora"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="PR and outreach Coordinator"
+        ImagePath="src/Images/Team/ArshArora.jpg"
+      />
+      <ProfileCard
+        title="Aditya Gupta"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="PR and outreach Coordinator"
+        ImagePath="src/Images/Team/AdityaGupta.jpg"
+      />
+      <ProfileCard
+        title="Devansh Agrawal"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="PR and outreach Coordinator"
+        ImagePath="src/Images/Team/DevanshAgrawalpicture.jpg"
+      />
+      </div>
+
+      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row mx-12'>
+      <ProfileCard
+        title="Manoj Kumar Reddy"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="PR and outreach Coordinator"
+        ImagePath="src/Images/Team/BapatuManojKumarReddy.jpg"
+      />
+      <ProfileCard
+        title="Arin Kulkarni"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Design Coordinator"
+        ImagePath="src/Images/Team/ArinKulkarni.jpg"
+      />
+      <ProfileCard
+        title="Banavath Vishnu"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Design Coordinator"
+        ImagePath="src/Images/Team/BanavathVishnu.jpg"
+      />
+      </div>
+
+      <div className='flex flex-col items-center max-w-[1240px] lg:flex-row mx-12'>
+      <ProfileCard
+        title="Tanish Samikommu"
+        gradient="from-[#D8B4FE] to-[#818CF8]"
+        Domain="Design Coordinator"
+        ImagePath="src/Images/Team/TanishReddy_.jpg"
+      />
+      </div>
+
 
     </div>
   )
